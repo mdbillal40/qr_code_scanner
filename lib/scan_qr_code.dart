4a3fa8ad7bc954df7f2d3b6b5e212788fb319f5c
mdbillal40 <mshuvon274@gmail.com>
2026-06-06 20:18:22 +0600

    Initial commit

---
+++ lib/scan_qr_code.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GenerateQRCode extends StatefulWidget {
+  const GenerateQRCode({super.key});
+
+  @override
+  State<GenerateQRCode> createState() => _GenerateQRCodeState();
+}
+
+class _GenerateQRCodeState extends State<GenerateQRCode> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
